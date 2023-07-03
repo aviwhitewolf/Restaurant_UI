0a6837b534e545851c3e48d2a0b5fc44dbeec25c
Avinash kumar <avinash.8894@hotmail.com>
2023-07-03 18:16:04 +0530

    Remove unused code

--- src/app/admin/dishes/dishes.module.ts
+++ src/app/admin/dishes/dishes.module.ts
@@ -6,7 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { ReusableModule } from 'src/app/reusable/reusable.module';
 import { DishRoutingModule } from './dish-routing.module';
-import { DragAndDropModule } from './drag-and-drop/drag-and-drop.module';
 import { DishVariationsModule } from './dish-variations/dish-variations.module';
 
 @NgModule({
@@ -21,7 +20,6 @@ import { DishVariationsModule } from './dish-variations/dish-variations.module';
     ReusableModule,
     DishRoutingModule,
     ReactiveFormsModule,
-    DragAndDropModule,
     DishVariationsModule
   ]
 })
